3ffa24aefcb63b9a633dace70939614b2102e223
ELCOT <ELCOT@Lenovo>
2022-05-14 13:09:59 +0530

    initial commit

---
+++ com.testng1/src/test/java/Testing1/com/testng1/sample_testng.java
@@ -0,0 +1,18 @@
+package Testing1.com.testng1;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class sample_testng {
+
+	public static WebDriver driver;
+
+	@Test
+	
+	private void pedi() {
+	System.out.println("jayseeelan");
+
+	}
+	
+
+}
